cfeafd6abddeb8d666602b1316e23164652e3bca
Shunsuke Kanda <shnsk.knd@gmail.com>
2022-08-11 18:15:48 +0900

    Add GitHub link (#13)
    * add github link
    
    * minor

--- goodname-wasm/src/cand_view.rs
+++ goodname-wasm/src/cand_view.rs
@@ -17,6 +17,8 @@ pub fn cand_view(props: &Props) -> Html {
                     <th>{"Name"}</th>
                     <th>{"Description"}</th>
                     <th>{"Score"}</th>
+                    <th>{"Google"}</th>
+                    <th>{"GitHub"}</th>
                 </tr>
             </thead>
             <tbody>
@@ -24,9 +26,11 @@ pub fn cand_view(props: &Props) -> Html {
                     for candidates.into_iter().enumerate().map(|(i, (cand, active, score))| html! {
                         <tr>
                             <td>{i+1}</td>
-                            <td class="cand-name"><a href={format!("https://www.google.com/search?q={}", cand.to_lowercase())} target="_blank">{cand}</a></td>
+                            <td class="cand-name">{cand.clone()}</td>
                             <td class="cand-desc">{active}</td>
                             <td>{score}</td>
+                            <td><a href={format!("https://www.google.com/search?q={}", cand.to_lowercase())} target="_blank"><span class="material-symbols-outlined">{"search"}</span></a></td>
+                            <td><a href={format!("https://github.com/search?q={}", cand.to_lowercase())} target="_blank"><span class="material-symbols-outlined">{"search"}</span></a></td>
                         </tr>
                     })
                 }
